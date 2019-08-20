a4c702f876d74161148ec378aaa1d7400394bea8
eduqg <eduardoqgomes@gmail.com>
2019-08-20 19:01:57 -0300

    Styling feed

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -58,7 +58,7 @@ export default class Feed extends Component {
                         </p>
                     </footer>
                 </article>
-                
+
             </section>
         )
     }
