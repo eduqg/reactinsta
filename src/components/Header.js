1d9bfd44a9d26cf2664a19adeb102614694cbac5
eduqg <eduardoqgomes@gmail.com>
2019-08-20 16:51:37 -0300

    Initializing header

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './Header.css';
+
+export default function Header() {
+    return (
+        <header id="main-header">
+            <div className="header-content" >
+                <img src="" alt="InstaRocket" />
+                <img src="" alt="Enviar publicação" />
+            </div>
+        </header>
+    );
+}
